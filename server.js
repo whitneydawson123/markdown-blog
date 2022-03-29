94370da91c493d43ade6fe2df25824cf8703d58f
Whitney Dawson <whitneydawson123@gmail.con>
2022-03-29 12:09:44 -0400

    updated

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const articleRouter = require('./routes/articles');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use('./articles', articleRouter);
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: Date.now(),
+        description: 'Test description'
+    }]
+    res.render('index', { articles: articles });
+});
+
+app.listen(5000);
